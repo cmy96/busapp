0e8d3abfd505cc41c8faa2af6bcabd98f98a8ceb
cmy96 <chuamingyu96@gmail.com>
2018-07-18 10:03:43 +0800

    Added in display of pyplot

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, BooleanField, SelectField, RadioField
+from wtforms.validators import DataRequired, ValidationError
+from wtforms_components import TimeField
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+class BusForm(FlaskForm):
+    bus = StringField('Bus number', validators=[DataRequired()])
+    timing = TimeField('What is the required timing', validators=[DataRequired()])
+    weekday = RadioField(choices=[("wd","Weekday"),("we","Weekend")])
+    submit = SubmitField('Submit')
+
+    def validate_bus(self,bus):
+        if bus.data not in ['10','158','12']:# dummy bus numbers
+            raise ValidationError("That is not a valid bus number")
+
+            
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,51 @@
+from flask import render_template, flash, redirect, url_for, request, make_response
 from app import app
-from flask import render_template,flash,redirect
-from app import app
-from app.forms import LoginForm
+from app.forms import BusForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
-
-@app.route('/login')
-def login():
-    form = LoginForm()
+    form = BusForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+        flash("Querying")
+        query = url_for("query",bus=form.bus.data, \
+        timing=form.timing.data,weekday=form.weekday.data)
+        return redirect(query)
+    return render_template('index.html',form = form,title = "Home page")
+
+@app.route('/query')
+def query():
+    # bus = request.args.get("bus")
+    # timing = request.args.get("timing")
+    # weekday = request.args.get('weekday')
+    return render_template("query.html")
+
+@app.route('/image.png')
+def image():
+    import datetime
+    from io import BytesIO
+    import random
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
